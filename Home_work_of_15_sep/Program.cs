4a6ebb8984cc6f0acc8dd85f84c96f3c6d41578f
abhayhp <dadhwal.abhay.abhay@gmail.com>
2022-09-16 10:29:08 +0530

    Add project files.

---
+++ Home_work_of_15_sep/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_work_of_15_sep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stnt st1 = new Stnt { Rollno  = 24, Name = "Abhay", Per = 85.05 };
+            Console.WriteLine(st1);
+    }
+    }
+}
---
+++ Home_work_of_15_sep/Stnt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_work_of_15_sep
+{
+    public class Stnt
+    {
+        private int rollno;
+        private string name;
+        private double per;
+
+        public int Rollno { get; set; }
+        public string Name { get; set; }
+        public double Per { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Rollno : {Rollno} \nName : {Name} \nPer : {Per}" ;
+        }
+    }
+}
